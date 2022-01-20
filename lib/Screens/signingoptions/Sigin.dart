da5195daec8ad4d1a65c5ef4b426545124298fa2
Dipendra-Raghav <raghavdip19@gmail.com>
2022-01-20 19:08:51 +0530

    button desogned

--- lib/Screens/signingoptions/Sigin.dart
+++ lib/Screens/signingoptions/Sigin.dart
@@ -1,3 +1,4 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -12,6 +13,29 @@ class _SignInState extends State<SignIn> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Sign in',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              FlatButton.icon(
+                color: Colors.white,
+                onPressed: () {
+                  print('Google sign in...');
+                },
+                icon: Icon(EvaIcons.google),
+                label: Text('Sign in with google'),
+              )
+            ],
+          ),
+        ),
         decoration: BoxDecoration(
             image: DecorationImage(
                 image: AssetImage('images/logo.png'),
