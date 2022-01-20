5e3aaf4bbcb0ff88ebed044aad6009f14a091605
Dipendra-Raghav <raghavdip19@gmail.com>
2022-01-20 18:27:52 +0530

    Sigin button navigation to the page

--- lib/Screens/landingPage.dart
+++ lib/Screens/landingPage.dart
@@ -1,4 +1,6 @@
+import 'package:course_selling/Screens/signingoptions/Sigin.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 class LandingPage extends StatefulWidget {
   @override
@@ -9,12 +11,47 @@ class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.redAccent,
-        centerTitle: true,
-        title: Text('Landing page'),
+      backgroundColor: Colors.black,
+      body: Container(
+        child: Center(
+          child: SizedBox(
+            height: 400.0,
+            width: 400.0,
+            child: Image.asset(
+              'images/logo.png',
+              fit: BoxFit.fitHeight,
+              colorBlendMode: BlendMode.darken,
+              color: Colors.black54,
+            ),
+          ),
+        ),
       ),
-      body: Container(),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.blue,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            MaterialButton(
+                onPressed: () {
+                  print('Pressed');
+                },
+                child: Text('Browse',
+                    style: TextStyle(
+                        color: Colors.grey.shade300, fontSize: 20.0))),
+            MaterialButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                      context,
+                      PageTransition(
+                          child: SignIn(),
+                          type: PageTransitionType.bottomToTop));
+                },
+                child: Text('Sign in',
+                    style:
+                        TextStyle(color: Colors.grey.shade300, fontSize: 20.0)))
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/Screens/signingoptions/Sigin.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class SignIn extends StatefulWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('images/logo.png'),
+                fit: BoxFit.fitHeight,
+                colorFilter:
+                    ColorFilter.mode(Colors.black54, BlendMode.darken))),
+      ),
+    );
+  }
+}
