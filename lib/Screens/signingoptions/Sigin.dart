cfc2af58c1d6e220382798a59c3bb29ae9a9f8c0
Dipendra-Raghav <raghavdip19@gmail.com>
2022-01-23 20:54:48 +0530

    create account button (7.10)

--- lib/Screens/signingoptions/Sigin.dart
+++ lib/Screens/signingoptions/Sigin.dart
@@ -1,5 +1,7 @@
+import 'package:course_selling/Screens/signingoptions/signUp.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({Key? key}) : super(key: key);
@@ -18,25 +20,54 @@ class _SignInState extends State<SignIn> {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
-                'Sign in',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold),
+              const Padding(
+                padding: EdgeInsets.only(bottom: 100.0),
+                child: Text(
+                  'Sign in',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold),
+                ),
               ),
-              FlatButton.icon(
-                color: Colors.white,
-                onPressed: () {
-                  print('Google sign in...');
-                },
-                icon: Icon(EvaIcons.google),
-                label: Text('Sign in with google'),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 15.0),
+                child: FlatButton.icon(
+                    color: Colors.white,
+                    onPressed: () {
+                      print('Google sign in...');
+                    },
+                    icon: Icon(EvaIcons.google),
+                    label: Text('Sign in with google')),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 100.0),
+                child: Row(
+                  children: [
+                    const Text(
+                      'New here?',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    MaterialButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            PageTransition(
+                                child: SignUp(),
+                                type: PageTransitionType.bottomToTop));
+                      },
+                      child: const Text(
+                        'Create an account',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
               )
             ],
           ),
         ),
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
             image: DecorationImage(
                 image: AssetImage('images/logo.png'),
                 fit: BoxFit.fitHeight,
