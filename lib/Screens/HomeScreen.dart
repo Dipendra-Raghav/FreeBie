30ce197030918c28c502295e7ac7e7f3bdf2d79c
Dipendra-Raghav <raghavdip19@gmail.com>
2022-01-24 22:44:53 +0530

    bottom bar done (5.28)

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 
 class Homescreen extends StatefulWidget {
@@ -11,8 +12,31 @@ class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
       body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.grey.shade800,
+        selectedIconTheme: IconThemeData(color: Colors.redAccent),
+        unselectedIconTheme: IconThemeData(color: Colors.white),
+        unselectedLabelStyle: TextStyle(color: Colors.white),
+        unselectedItemColor: Colors.white,
+        iconSize: 26.0,
+        selectedFontSize: 14.0,
+        unselectedFontSize: 12.0,
+        currentIndex: 0,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.star), title: Text('Featured')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search), title: Text('Search')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.music_video), title: Text('My Courses')),
+          BottomNavigationBarItem(
+              icon: Icon(EvaIcons.heartOutline), title: Text('Wishlist')),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_box), title: Text('Account'))
+        ],
+      ),
     );
   }
 }
